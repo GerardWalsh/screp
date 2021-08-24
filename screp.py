dfebb4e3f9f54a30dd851cabf6ef665c7e2c0e15
GerardWalsh <gerar9207@gmail.com>
2021-08-24 16:33:37 +0200

    86 scraper for autotrader, multiple pages

--- screp.py
+++ screp.py
@@ -5,25 +5,70 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 opts = Options()
 opts.add_argument("--headless")
 chrome_driver_path = Path(os.getcwd()) / "chromedriver_linux64/chromedriver"
 
 driver = webdriver.Chrome(options=opts, executable_path=chrome_driver_path)
-driver.get("https://www.webuycars.co.za/buy-a-car?q=86")
-
+driver.get("https://www.autotrader.co.za/cars-for-sale/toyota/86?sortorder=PriceLow")
 soup = BeautifulSoup(driver.page_source, "html.parser")
-eight_sixes = soup.find_all("div", {"class": "filter-item-media"})
-print(f"we Have {len(eight_sixes)} examples")
+pages = soup.find_all("li", {"class": "e-page-number"})
 
-first_love = eight_sixes[0]
-first_loves_address = first_love.select_one("a")["href"]
-we_buy_cars_home_prefix = "https://www.webuycars.co.za"
+price = []
+mileage = []
+year = []
 
-url = we_buy_cars_home_prefix + first_loves_address
-response = requests.get(url)
-new_soup = BeautifulSoup(driver.page_source, "html.parser")
-price = new_soup.find("div", {"class": "d-flex align-items-center mr-1"}).get_text().replace("\xa0", "")
+for page_number in range(1, len(pages) + 1):
+    if page_number == 0:
+        url = "https://www.autotrader.co.za/cars-for-sale/toyota/86?sortorder=PriceLow"
+    else:
+        url = f"https://www.autotrader.co.za/cars-for-sale/toyota/86?pagenumber={page_number}&sortorder=PriceLow"
+    # driver.get(
+    #     f"https://www.autotrader.co.za/cars-for-sale/toyota/86?pagenumber={page_number}&sortorder=PriceLow"
+    # )
 
-print(f'First GT86 costs {price}')
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+
+    meta = soup.find_all("span", {"class": "e-icons"})
+    prices = soup.find_all("span", {"class": "e-price"})
+
+    for ad in zip(prices, meta):
+        try:
+            price.append(int(ad[0].text.replace("R ", "").replace("\xa0", "")))
+            year.append(int(ad[1].text[0:4]))
+            mileage.append(int(ad[1].text[4:11].replace("\xa0", "")))
+        except:
+            pass
+
+
+# driver.get(
+#     "https://www.autotrader.co.za/cars-for-sale/toyota/86?pagenumber=2&sortorder=PriceLow"
+# )
+
+# soup = BeautifulSoup(driver.page_source, "html.parser")
+# meta = soup.find_all("span", {"class": "e-icons"})
+# prices = soup.find_all("span", {"class": "e-price"})
+
+# for ad in zip(prices, meta):
+#     try:
+#         price.append(int(ad[0].text.replace("R ", "").replace("\xa0", "")))
+#         year.append(int(ad[1].text[0:4]))
+#         mileage.append(int(ad[1].text[4:11].replace("\xa0", "")))
+#     except:
+#         pass
+
+
+sns.scatterplot(x=mileage, y=price, hue=year, palette="bright")
+plt.xlabel("Mileage [km]")
+plt.ylabel("Price [R]")
+# plt.legend("Year")
+plt.title("Price vs mileage for the Toyota 86, on Autotrader")
+plt.show()
+
+import ipdb
+
+ipdb.set_trace()
