924845cbf5912ad5f846f80ded5efab23bf8fd12
Gerard Walsh <gerard9207@gmail.com>
2024-07-25 08:12:21 +0200

    round last_seen time

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def main():
 
     df = pd.read_csv("frontend_data.csv", index_col=0)
     df.time_online = pd.to_timedelta(df.time_online).round("1 min")
-    # df.last_seen = pd.to_timedelta(df.last_seen).round("1 min")
+    df.last_seen = pd.to_datetime(df.last_seen).round("60 min")
     filter_map = {
         "model": [
             " ".join(x).title()
