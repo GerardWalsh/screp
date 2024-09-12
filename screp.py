8e972029023ff20f023b52ed19609c68750de219
Gerard Walsh <gerard9207@gmail.com>
2024-09-12 09:54:02 +0200

    tidy up autotrade scrape

--- screp.py
+++ screp.py
@@ -12,11 +12,12 @@ from utils import (
     get_ad_details,
     insert_ads,
     get_soup,
+    get_all_page_ads
 )
 
 target_site = "autotrader"
 url_patterns = {
-    "autotrader": "https://www.autotrader.co.za/cars-for-sale/{manufacturer}/{model}?pagenumber=1"
+    "autotrader": "https://www.autotrader.co.za/cars-for-sale/{}/{}?pagenumber={}"
 }
 data = load_yaml(f"scrape_configs/{target_site}_scrape_targets.yaml")
 driver = setup_driver()
@@ -25,22 +26,26 @@ model_url = url_patterns[target_site]
 for manufacturer in data.keys():
     for model in data[manufacturer]:
         datas = []
-        model_url = model_url.format(manufacturer, model)
+        model_url = url_patterns[target_site].format(manufacturer, model, 1)
         soup = get_soup(driver, model_url, pause=False)
-        pages = len(soup.find_all(*find_total_pages(soup, "autotrader")))
         ad_count = int(soup.select(find_total_ads(soup, "autotrader"))[0].text)
-        print(f"Found {ad_count} ads for {model}")
+        pages = int(soup.select(find_total_pages(soup, "autotrader"))[-1].text)
+        print(f"Found {pages} pages and {ad_count} ads for {model}.")
         for i in range(pages):
             if i != 0:
                 print(f"Getting page {i+1} data")
-                model_url = f"https://www.autotrader.co.za/cars-for-sale/{manufacturer}/{model}?pagenumber={i+1}"
+                model_url = url_patterns[target_site].format(manufacturer, model, i+1)
                 soup = get_soup(driver, model_url)
 
-            for ad_soup in soup.select('[class^="b-result-tile__"]'):
+            for ad_soup in get_all_page_ads(soup, target_site):
                 try:
                     datas.append(get_ad_details(ad_soup))
                 except:
                     pass
         datas = pd.DataFrame(datas)
         datas["date_retrieved"] = str(datetime.now())
-        insert_ads(db_name="listings", data=datas)
+        datas["manufacturer"] = manufacturer
+        datas["model"] = model
+        insert_ads(db_name="listing.db", data=datas)
+
+driver.close()
