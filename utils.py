d1cff4c58258207cdf457475daad14d89d6f8a38
GerardWalsh <gerard9207@gmail.com>
2024-09-13 12:26:27 +0200

    better scraping of wbc, checking if no ads

--- utils.py
+++ utils.py
@@ -23,14 +23,15 @@ def setup_driver():
 
 def find_total_pages(soup, site):
     if site == "autotrader":
-        return  int(soup.select('[classs^="e-page-number"]')[-1].text)
+        return int(soup.select('[classs^="e-page-number"]')[-1].text)
     elif site == "wbc":
         if any(soup.find_all("ul", class_="pagination")):
             return ceil(
                 int(
                     [
-                        x.text for x in soup.find_all("div", class_="text-center") if "Showing 1" in x.text
-                        
+                        x.text
+                        for x in soup.find_all("div", class_="text-center")
+                        if "Showing 1" in x.text
                     ][0]
                     .split("of ")[-1]
                     .replace(")", "")
@@ -43,20 +44,22 @@ def find_total_pages(soup, site):
 
 def find_total_ads(soup, site):
     if site == "autotrader":
-        total=  int(soup.select('[class^="e-results-total__"]')[0].text)
+        total = int(soup.select('[class^="e-results-total__"]')[0].text)
     elif site == "wbc":
         if any(soup.find_all("ul", class_="pagination")):
             return int(
-                    [
-                        x.text for x in soup.find_all("div", class_="text-center") if "Showing 1" in x.text
-                        
-                    ][0]
-                    .split("of ")[-1]
-                    .replace(")", "")
-                )
+                [
+                    x.text
+                    for x in soup.find_all("div", class_="text-center")
+                    if "Showing 1" in x.text
+                ][0]
+                .split("of ")[-1]
+                .replace(")", "")
+            )
         else:
             return 1
     return total
+
 
 def get_ad_containers(soup, site):
     patterns = {"autotrader": '[class^="b-result-tile__"]'}
@@ -68,14 +71,15 @@ def get_ad_details(soup, site):
     if site == "autotrader":
         data["ad_id"] = soup.find("a").get("href").split("?")[0].split("/")[-1]
         for component in ["title", "dealer", "suburb", "price"]:
-            data[component] = soup.select(f'[class^="e-{component}__"]')[0].text.replace(
-                "\xa0", " "
-            )
+            data[component] = soup.select(f'[class^="e-{component}__"]')[
+                0
+            ].text.replace("\xa0", " ")
         for i, component in enumerate(["transmission", "mileage"]):
-            data[component] = soup.select('[class^="e-summary-icon"]')[-1 - i].text.replace(
-                "\xa0", " "
-            )
+            data[component] = soup.select('[class^="e-summary-icon"]')[
+                -1 - i
+            ].text.replace("\xa0", " ")
     elif site == "wbc":
+        data["ad_id"] = soup.find("div", class_="grid-card").get("id").split("-")[-1]
         data["title"] = soup.select('[class^="description"]')[0].text
         data["dealer"] = "wbc"
         data["suburb"] = soup.select('[class^="chip-text"]')[2].text
@@ -109,3 +113,8 @@ def get_all_page_ads(page_soup, site):
         return page_soup.select('[class^="b-result-tile__"]')
     elif site == "wbc":
         return page_soup.select('[class^="m-2 grid-card-container"]')
+
+
+def any_ads(page_soup, site):
+    if site == "wbc":
+        return not any(page_soup.select('[class^="no-results-message"]'))
